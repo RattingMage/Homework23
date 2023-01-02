8f9e44880c3a440664bb51913052caf85e557728
andry <mukhin2019@gmail.com>
2023-01-02 19:21:34 +0300

    with docker-compose

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
+import os
+
 from flask import Flask, request, jsonify
+
+from db import db
 from schemas import ParamsListSchema
 from marshmallow import ValidationError
 
 from utils import build_query
 
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
 
 
 @app.route("/perform_query", methods=["POST"])
@@ -26,6 +39,17 @@ def perform_query():
         result = build_query(cmd=query["cmd"], param=query["value"], data=result)
 
     return jsonify(result), 200
+
+
+@app.route("/test_db")
+def test_db():
+    result = db.session.execute("""
+        SELECT 1
+    """).scalar()
+
+    return jsonify({
+        'result': result
+    })
 
 
 if __name__ == "__main__":
